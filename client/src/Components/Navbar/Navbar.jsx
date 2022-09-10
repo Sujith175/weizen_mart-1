87afa3ab2578524edcc4487d7d17f38b43db0606
Sujith175 <sujithkumar.sk175@gmail.com>
2022-09-11 00:31:43 +0530

    refactor:corrected css on landing page

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,40 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./NavbarElements.scss";
 import { Link, Outlet } from "react-router-dom";
-import SearchIcon from '@mui/icons-material/Search';
-
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  NavbarContainer,
+  NavbarLanguage,
+  NavLogo,
+  NavMenuItem,
+  NavMenuLink,
+  NavRight,
+  NavSearchContainer,
+  NavSearchInput,
+  WrapperContainer,
+  NavCenter,
+  NavbarLeft,
+  LogoutButton,
+} from "./NavbarElements";
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="left">
-          <span className="language">EN</span>
-          <div className="search-container">
-            <input type="search" className="search-input" />
-            <SearchIcon style={{color:'grey',fontSize:'16px'}}/>
-          </div>
-        </div>
-        <div className="center">
-          <h1 className="logo">WEIZEN MART</h1>
-        </div>
-        <div className="right">
-          <div className="menuitem">
-            <Link to="/signup" className="menu-link">
-              Register
-            </Link>
-          </div>
-          <div className="menuitem">
-            <Link to="/login" className="menu-link">
-              Login
-            </Link>
-          </div>
-          <div className="menuitem">
-            <button>Logout</button>
-          </div>
-        </div>
-      </div>
+    <>
+      <NavbarContainer>
+        <WrapperContainer>
+          <NavbarLeft>
+            <NavbarLanguage>EN</NavbarLanguage>
+            <NavSearchContainer>
+              <NavSearchInput />
+              <SearchIcon style={{ color: "grey", fontSize: "16px" }} />
+            </NavSearchContainer>
+          </NavbarLeft>
+          <NavCenter>
+            <NavLogo>WEIZEN MART</NavLogo>
+          </NavCenter>
+          <NavRight>
+            <NavMenuItem>
+              <NavMenuLink to="/signup">Register</NavMenuLink>
+            </NavMenuItem>
+            <NavMenuItem>
+              <NavMenuLink to="/login">Login</NavMenuLink>
+            </NavMenuItem>
+            <NavMenuItem>
+              <LogoutButton>Logout</LogoutButton>
+            </NavMenuItem>
+          </NavRight>
+        </WrapperContainer>
+      </NavbarContainer>
       <Outlet />
-    </div>
+    </>
   );
 };
 
---
+++ client/src/Components/Navbar/NavbarElements.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+export const NavbarContainer = styled.div`
+  height: 60px;
+  @media only screen and (max-width: 380px) {
+    height: 50px;
+  }
+`;
+
+export const WrapperContainer = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media only screen and (max-width: 380px) {
+    padding: 10px 0px;
+  }
+`;
+
+export const NavbarLeft = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+export const NavbarLanguage = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  @media only screen and (max-width: 380px) {
+    display: none;
+  }
+`;
+
+export const NavSearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+export const NavSearchInput = styled.input`
+  border: none;
+  @media only screen and (max-width: 380px) {
+    width: 50px;
+  }
+`;
+
+export const NavCenter = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+export const NavLogo = styled.h1`
+  font-weight: bold;
+  @media only screen and (max-width: 380px) {
+    font-size: 24px;
+  }
+`;
+
+export const NavMenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+export const NavRight = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+export const NavMenuLink = styled(Link)`
+  display: inline-block;
+  text-decoration: none;
+  color: black;
+  border: 1px solid #fff;
+  padding: 12px 34px;
+  font-size: 13px;
+  background: transparent;
+  position: relative;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid #a9740e;
+    background: #a9740e;
+    transition: 1s;
+  }
+`;
+export const LogoutButton = styled.button`
+  display: inline-block;
+  text-decoration: none;
+  color: black;
+  border: 1px solid #fff;
+  padding: 12px 34px;
+  font-size: 13px;
+  background: transparent;
+  position: relative;
+
+  cursor: pointer;
+  &:hover {
+    border: 1px solid #a9740e;
+    background: #a9740e;
+    transition: 1s;
+  }
+`;
